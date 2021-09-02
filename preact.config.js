27df42fa64eca7e2226cefad7510958f6cbc27ec
Brandon Aaron <hello.brandon@aaron.sh>
2021-09-02 09:57:19 -0400

    big dependency update

--- preact.config.js
+++ preact.config.js
@@ -1,11 +1,13 @@
-const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 export default (config, env, helpers) => {
-  const context = path.resolve(__dirname, 'src', 'assets')
-  config.plugins.push(new CopyWebpackPlugin([{ context, from: 'robots.txt' }]))
-  config.plugins.push(new CopyWebpackPlugin([{ context, from: 'browserconfig.xml' }]))
-  config.plugins.push(new CopyWebpackPlugin([{ context, from: 'CNAME' }]))
+  config.plugins.push(new CopyWebpackPlugin({
+    patterns: [
+      { from: 'assets/robots.txt' },
+      { from: 'assets/browserconfig.xml' },
+      { from: 'assets/CNAME' }
+    ]
+  }))
 
   return config
 }
