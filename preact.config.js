a2ae8de03ca6640738574432d2c9a8ac7c6dbbec
Brandon Aaron <hello.brandon@aaron.sh>
2021-09-02 09:49:47 -0400

    update eslint

--- preact.config.js
+++ preact.config.js
@@ -1,9 +1,11 @@
+const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 export default (config, env, helpers) => {
-  config.plugins.push(new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: 'robots.txt' }]))
-  config.plugins.push(new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: 'browserconfig.xml' }]))
-  config.plugins.push(new CopyWebpackPlugin([{ context: `${__dirname}/src/assets`, from: 'CNAME' }]))
+  const context = path.resolve(__dirname, 'src', 'assets')
+  config.plugins.push(new CopyWebpackPlugin([{ context, from: 'robots.txt' }]))
+  config.plugins.push(new CopyWebpackPlugin([{ context, from: 'browserconfig.xml' }]))
+  config.plugins.push(new CopyWebpackPlugin([{ context, from: 'CNAME' }]))
 
   return config
 }
